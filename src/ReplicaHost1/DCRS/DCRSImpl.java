f6e7488870a2cbd7adecb23ba2833c2d80f7bb43
lqw1111 <369487836luo@gmail.com>
2018-11-28 22:29:55 -0500

    fix sort issue

--- src/ReplicaHost1/DCRS/DCRSImpl.java
+++ src/ReplicaHost1/DCRS/DCRSImpl.java
@@ -149,7 +149,6 @@ public class DCRSImpl {
             }
 
             String[] courses = courseAvailibleList.split(" ");
-            Arrays.sort(courses);
             for (String course :
                     courses) {
                 if (!course.equals("")){
@@ -161,7 +160,9 @@ public class DCRSImpl {
             e.printStackTrace();
         }
 
-        return translateStringArray(courseList);
+        String[] result = translateStringArray(courseList);
+        Arrays.sort(result);
+        return result;
     }
 
     private String[] translateStringArray(List<String> courseList) {
@@ -291,8 +292,9 @@ public class DCRSImpl {
             String str = course.getCourseName() + "--" + course.getSemester();
             res.add(str);
         }
-
-        return translateStringArray(res);
+        String[] result = translateStringArray(res);
+        Arrays.sort(result);
+        return result;
     }
 
     public String swapCourse(String studentID, String newCourseID, String oldCourseID) {
